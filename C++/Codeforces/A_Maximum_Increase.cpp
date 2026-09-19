df11102cd5e9914cbc1c6496080ce416ceaec53c
Zuairia Akter <zuairiaakter.cse@gmail.com>
2026-09-19 20:33:20 +0600

    Codeforces - A_Maximum_Increase

---
+++ C++/Codeforces/A_Maximum_Increase.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int a[n];
+    for(int i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+
+    int length=1;
+    int maxlength=1;
+
+    for(int i=0; i<n-1; i++)
+    {
+        if(a[i]<a[i+1])
+        {
+            length++;
+        }
+        else
+        {
+            length = 1;
+        }
+        maxlength = max(maxlength, length);
+    }
+    cout<<maxlength<<endl;
+}
